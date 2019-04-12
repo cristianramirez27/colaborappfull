7eb1cd93942aa56d874546273adec9690027cc1a
Fausto <f.lima.rodriguez@gmail.com>
2019-04-12 10:56:04 -0500

    Corrección en persistencia del menu (Icono de CartasLaborales)

--- app/src/main/java/com/coppel/rhconecta/dev/views/utils/MenuUtilities.java
+++ app/src/main/java/com/coppel/rhconecta/dev/views/utils/MenuUtilities.java
@@ -22,11 +22,38 @@ import io.realm.RealmList;
 public class MenuUtilities {
 
     public static List<HomeMenuItem> getHomeMenuItems(Context context, String email, boolean isSlide,int[] notifications) {
+
+        /*Setamos el menu por default*/
+        List<HomeMenuItem> listMenuDefault = Arrays.asList(
+                new HomeMenuItem(context.getString(R.string.notices), AppConstants.OPTION_NOTICE,notifications[0]),
+                new HomeMenuItem(context.getString(R.string.payroll_voucher), AppConstants.OPTION_PAYROLL_VOUCHER),
+                new HomeMenuItem(context.getString(R.string.benefits), AppConstants.OPTION_BENEFITS),
+                new HomeMenuItem(context.getString(R.string.loan_saving_fund), AppConstants.OPTION_SAVING_FUND),
+                new HomeMenuItem(context.getString(R.string.employment_letters), AppConstants.OPTION_LETTERS),
+                new HomeMenuItem(context.getString(R.string.visionaries), AppConstants.OPTION_VISIONARIES,notifications[1]));
+
         UserPreference userPreferences = RealmHelper.getUserPreferences(email);
         List<HomeMenuItem> homeMenuItems = new ArrayList<>();
         if (userPreferences != null && userPreferences.getMenuItems() != null && userPreferences.getMenuItems().size() > 0 && !isSlide) {
 
             RealmList<HomeMenuItem> menus =  userPreferences.getMenuItems();
+
+            List<String> listMenuSavedtTAG = new ArrayList<>();
+            for(HomeMenuItem item : menus){
+                listMenuSavedtTAG.add(item.getTAG());
+            }
+
+
+
+            /**Validamos si se agregaron todos los items del menu en las preferencias, en caso contrario, lo agregamos*/
+            if(listMenuSavedtTAG.size() < listMenuDefault.size()){
+                for (HomeMenuItem homeMenuItem : listMenuDefault){
+                    if(!listMenuSavedtTAG.contains(homeMenuItem.getTAG())){
+                        menus.add(homeMenuItem);
+                    }
+                }
+            }
+
             Realm realm = Realm.getDefaultInstance();
             if (!realm.isInTransaction()) realm.beginTransaction();
             for (int i =0;i<menus.size();i++){
@@ -54,13 +81,7 @@ public class MenuUtilities {
             if (isSlide) {
                 homeMenuItems.add(new HomeMenuItem(context.getString(R.string.title_home), AppConstants.OPTION_HOME));
             }
-            homeMenuItems.addAll(Arrays.asList(
-                    new HomeMenuItem(context.getString(R.string.notices), AppConstants.OPTION_NOTICE,notifications[0]),
-                    new HomeMenuItem(context.getString(R.string.payroll_voucher), AppConstants.OPTION_PAYROLL_VOUCHER),
-                    new HomeMenuItem(context.getString(R.string.benefits), AppConstants.OPTION_BENEFITS),
-                    new HomeMenuItem(context.getString(R.string.loan_saving_fund), AppConstants.OPTION_SAVING_FUND),
-                    new HomeMenuItem(context.getString(R.string.employment_letters), AppConstants.OPTION_LETTERS),
-                    new HomeMenuItem(context.getString(R.string.visionaries), AppConstants.OPTION_VISIONARIES,notifications[1])));
+            homeMenuItems.addAll(listMenuDefault);
         }
         return homeMenuItems;
     }
