ed7415eea55fba6c0659c81fdd25c44e0d7d45d3
Usuario <omar.lindoro@coppel.com>
2020-06-08 11:55:40 -0600

    Reactivando bloqueo de opcion de QR (08/06/20)

--- app/src/main/java/com/coppel/rhconecta/dev/views/utils/MenuUtilities.java
+++ app/src/main/java/com/coppel/rhconecta/dev/views/utils/MenuUtilities.java
@@ -194,11 +194,9 @@ public class MenuUtilities {
         for (HomeMenuItem item : homeMenuItems) {
             if(item.getTAG().equals(OPTION_COLLABORATOR_AT_HOME) && blockStayHome)
                 continue;
-/*
+
             if(item.getTAG().equals(OPTION_QR_CODE) && blockQr)
                 continue;
-
- */
 
             if(item.getTAG().equals(OPTION_COVID_SURVEY) && blockCovidSurvey)
                 continue;
