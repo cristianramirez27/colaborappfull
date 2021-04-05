1408d33fc347a9645605210113058f8af4ebf127
RobertoMartinezCS <roberto.mtz.med.cs@gmail.com>
2021-04-04 21:06:54 -0500

    Se da solución a la tarea 33 Banners-La APP se esta desbordando

--- app/src/main/java/com/coppel/rhconecta/dev/presentation/home/HomeMainFragment.java
+++ app/src/main/java/com/coppel/rhconecta/dev/presentation/home/HomeMainFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -366,9 +367,14 @@ public class HomeMainFragment
                 try {
                     startActivity(intent);
                 } catch (ActivityNotFoundException ex) {
-                    // Chrome browser presumably not installed so allow user to choose instead
-                    intent.setPackage(null);
-                    startActivity(intent);
+                    try {
+                        // Chrome browser presumably not installed so allow user to choose instead
+                        intent.setPackage(null);
+                        startActivity(intent);
+                    } catch (Exception exception) {
+                        String message = getString(R.string.invalid_url_link);
+                        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         };
