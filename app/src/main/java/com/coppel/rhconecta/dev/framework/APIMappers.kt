e06fb3f31f214107af71b4df6d0f98a1ccb1ee48
Aldo Estrada <aldoget1992@gmail.com>
2022-03-25 13:42:18 -0600

    1-MOVIMIENTOS Fixed error in backend

--- app/src/main/java/com/coppel/rhconecta/dev/framework/APIMappers.kt
+++ app/src/main/java/com/coppel/rhconecta/dev/framework/APIMappers.kt
@@ -4,13 +4,17 @@ import com.coppel.rhconecta.dev.framework.movements.Response
 import com.coppel.rhconecta.dev.views.fragments.fondoAhorro.movements.model.Movement
 import com.coppel.rhconecta.dev.views.utils.TextUtilities
 
+const val ERROR_BACKEND = 100
+
 fun Response.toMovementsDomainList(): List<Movement> = dataList.map {
     it.run {
         Movement(
             concept = concept.trim(),
             date = date,
             invoice = invoice,
-            amount = TextUtilities.getNumberInCurrencyFormat(amount)
+            //This is a client based request, this needs to be fixed in the backend,
+            // but it was agreed that this should be implemented in the frontend
+            amount = TextUtilities.getNumberInCurrencyFormat(amount / ERROR_BACKEND)
         )
     }
 }
